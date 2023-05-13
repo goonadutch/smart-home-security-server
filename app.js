f1f5db990dcd1870d84009000add3c6c67f8c428
goonadutch <goonadutch@gmail.com>
2023-05-13 05:49:16 +0530

    Added route to update distance (in memory)

--- app.js
+++ app.js
@@ -9,9 +9,16 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "public")));
 
+let distance = 0
 
 app.get("/", (_, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"))
+})
+
+
+app.get("/update/:distance", (req, res) => {
+    distance = parseInt(req.params.distance)
+    res.status(200).send("set distance to " + distance)
 })
 
 //The 404 Route (ALWAYS Keep this as the last route)
